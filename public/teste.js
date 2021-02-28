1117ae579554407762286bd2609803e41a07c5e2
Marcelo <marcelovalt@gmail.com>
2021-02-28 11:14:57 -0300

    bugfix test js

--- public/teste.js
+++ public/teste.js
@@ -1,5 +1,6 @@
 const fetchServerData = async() => {
     const dados = await axios.get("http://localhost:5001/music", {})
+    console.log(dados)
     if(dados.data.title) {
         document.getElementById("music").innerHTML = `Tocando Agora: ${dados.data.title}`;
     }
