35a4cfa1b6041955f629f5ed55403a5e3c7f529d
Marcelo <marcelovalt@gmail.com>
2021-02-28 11:22:13 -0300

    bugfix

--- public/teste.js
+++ public/teste.js
@@ -1,5 +1,5 @@
 const fetchServerData = async() => {
-    const dados = await axios.get("http://localhost:5001/music", {})
+    const dados = await axios.get("https://botlifeteam.herokuapp.com/music", {})
     console.log(dados)
     if(dados.data.title) {
         document.getElementById("music").innerHTML = `Tocando Agora: ${dados.data.title}`;
@@ -7,3 +7,5 @@ const fetchServerData = async() => {
     setTimeout(fetchServerData, 30000);
 }
 fetchServerData();
+//http://localhost:5001/music
+//https://botlifeteam.herokuapp.com/music
